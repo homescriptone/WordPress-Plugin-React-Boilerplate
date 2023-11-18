89dd968652d8c06154ec4fdf32a19ac4869f80d2
Emmanuel A <emmanueladekplovi1@gmail.com>
2023-11-18 15:30:12 +0100

    Change webpack configuration

--- plugin-name/webpack.config.js
+++ plugin-name/webpack.config.js
@@ -1,6 +1,6 @@
 const defaults = require('@wordpress/scripts/config/webpack.config');
 
-defaults['entry'] = ['./admin/src/plugin-name-admin.js', './admin/src/plugin-name-public.js'];
+defaults['entry'] = ['admin/src/plugin-name-admin.js', 'public/src/plugin-name-public.js'];
 module.exports = {
   ...defaults,
   externals: {
