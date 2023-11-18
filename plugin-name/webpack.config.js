9e1fe5972ff588e25740356a1f7d7337cb7174a5
Emmanuel A <emmanueladekplovi1@gmail.com>
2023-11-18 16:06:55 +0100

    Change the webpack build dist

--- plugin-name/webpack.config.js
+++ plugin-name/webpack.config.js
@@ -1,8 +1,8 @@
 const defaults = require('@wordpress/scripts/config/webpack.config');
 
 defaults['entry'] = {
-    admin : { import : './admin/src/plugin-name-admin.js', filename: './admin/dist/[name].js' },
-    public : { import : './public/src/plugin-name-public.js', filename: './public/dist/[name].js' },
+  admin : { import : './admin/src/plugin-name-admin.js', filename: '../admin/dist/plugin-name-admin.js' },
+  public : { import : './public/src/plugin-name-public.js', filename: '../public/dist/plugin-name-public.js' },
 }
 
 module.exports = {
