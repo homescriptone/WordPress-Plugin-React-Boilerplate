de19b7794ae276b48a6250f37e154b3bebf8d5ec
Emmanuel A <emmanueladekplovi1@gmail.com>
2023-11-18 15:43:32 +0100

    Change repo architecture

--- plugin-name/webpack.config.js
+++ plugin-name/webpack.config.js
@@ -1,10 +1,32 @@
 const defaults = require('@wordpress/scripts/config/webpack.config');
 
-defaults['entry'] = ['admin/src/plugin-name-admin.js', 'public/src/plugin-name-public.js'];
+var admin = Object.assign({}, defaults, {
+    name: "admin",
+    entry: './admin/src/plugin-name-admin.js',
+    output: {
+       path: "./admin/dist",
+       filename: "plugin-name-admin.js"
+    },
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+      },
+});
+
+var public = Object.assign({}, defaults, {
+    name: "public",
+    entry: './public/src/plugin-name-admin.js',
+    output: {
+       path: "./public/dist",
+       filename: "plugin-name-public.js"
+    },
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+      },
+});
+
+
 module.exports = {
-  ...defaults,
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
-  },
+  admin, public
 }; 
