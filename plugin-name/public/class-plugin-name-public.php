0acf32fc659e13332a063097b4e68c7ed41ed56b
Emmanuel A <emmanueladekplovi1@gmail.com>
2023-11-18 14:41:02 +0100

    Ajouter les bases pour le support pour ReactJS

--- plugin-name/public/class-plugin-name-public.php
+++ plugin-name/public/class-plugin-name-public.php
@@ -96,7 +96,7 @@ class Plugin_Name_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/plugin-name-public.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/plugin-name-public.js', array( 'wp-element' ), $this->version, false );
 
 	}
 
